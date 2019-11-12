056c08631aef647173b5e6c531be4a09b7df97a6
Jeppe Olsen <jmbols@fysik.dtu.dk>
2019-11-12 11:31:22 +0000

    Updated the test so it matches with the updated classes

--- tests/test3a-intersection-outboard.py
+++ tests/test3a-intersection-outboard.py
@@ -9,13 +9,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-from vita.modules.fiesta import Fiesta
+from vita.modules.fiesta import FieldLine
 from vita.modules.utils import intersection
 
 #filepath = "T:\\USERS\\J_Wood\\STF1_equilibriums\\export_R200.mat"
-filepath = "/home/daniel.iglesias/Simulations/plasma_scenarios/ST-F1/ST200/python_responses/export_R200.mat"
-field_line = Fiesta(filepath)
-R = field_line.getMidplaneLCFS()
+#filepath = "/home/daniel.iglesias/Simulations/plasma_scenarios/ST-F1/ST200/python_responses/export_R200.mat"
+filepath = '/home/jmbols/Postdoc/export_R200.mat'
+field_line = FieldLine(filepath)
+R = field_line.fiesta_equil.get_midplane_lcfs()
 r0 = 3.05
 rf = 3.1
 midplane_range = np.linspace(r0,rf,3)
@@ -27,7 +28,7 @@ for r in midplane_range:
 print(lengths)
 field_line_dict = []
 for idx, val in enumerate(points):
-    field_line_dict.append( field_line.followFieldinPlane(val, lengths[idx]) )
+    field_line_dict.append(field_line.follow_field_in_plane(val, lengths[idx]))
 #f, ax = plt.subplots(1)
 #for i in field_line_dict:
 #    ax.plot(i['R'],i['Z'])
@@ -47,16 +48,17 @@ divertor_x = np.array([2.15, 2.15, 2.27])
 divertor_y = np.array([-2.9, -3.25, -3.6])
 divertor_xy = np.array([divertor_x, divertor_y])
 
-result = [intersection(i, divertor_xy) for i in field_lines]
-x_p,y_p = zip (*result)
+result = np.array([intersection(i, divertor_xy) for i in field_lines])
+(x_p, y_p) = zip (*result[:,1])
 
 for i in field_line_dict:
     plt.plot(i['R'],i['Z'],c='r')
+plt.plot(field_line.fiesta_equil.r_limiter, field_line.fiesta_equil.z_limiter)
 plt.plot(divertor_x,divertor_y,c='g')
 plt.plot(x_p,y_p,'*k')
 plt.gca().set_aspect('equal', adjustable='box')
 plt.gca().set_ylim([-4,-2.5])
-plt.show(block=True)
+plt.show()#block=True)
 
 print(midplane_range)
 fx = []
