43eef688c528785d5c2b220942fd35ebd983e5a9
Jeppe Olsen <jmbols@fysik.dtu.dk>
2019-11-19 15:17:12 +0000

    Fixed a bug where it was calling the old function name and updated to fit with PEP8

--- vita/vitarunner.py
+++ vita/vitarunner.py
@@ -9,43 +9,51 @@ Created on Sun Oct 20 18:28:22 2019
 import os
 
 def is_valid_file(parser, arg):
-  if not os.path.exists(arg):
-    parser.error("The file %s does not exist!" % arg)
-  else:
-    return open(arg, 'r')  # return an open file handle
+    '''
+    Function for checking it a file exists
 
+    Input:  parser, an argparse.ArgumentParser object for parsing the terminal input
+            arg,    a terminal input with the file to load
+
+    return: open(arg, 'r'), an open file handle if file exists, else -1
+    '''
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+        return -1
+
+    return open(arg, 'r')
 
 if __name__ == "__main__":
-  import json
-  import argparse
-  from vita.controller.midplane_power import *
+    import json
+    import argparse
+    from vita.controller.midplane_power import run_midplane_power
 
-  parser = argparse.ArgumentParser(description='Run VITA controller processes in batch mode.\
-                                   Input is read from a JSON file (required).')
-  parser.add_argument("file",
-                      help="input file with JSON data for running simulations", 
-                      metavar="InputFile",
-                      type=lambda x: is_valid_file(parser, x))
-  args = parser.parse_args()
+    PARSER = argparse.ArgumentParser(description='Run VITA controller processes in batch mode.\
+                                         Input is read from a JSON file (required).')
+    PARSER.add_argument("file",
+                        help="input file with JSON data for running simulations",
+                        metavar="InputFile",
+                        type=lambda x: is_valid_file(PARSER, x))
 
-  print(args.file.name)
+    ARGS = PARSER.parse_args()
 
-  with open(args.file.name, 'r') as fh:
-      vita_input = json.load(fh)
+    print(ARGS.file.name)
 
-  print()
-  print(vita_input['run_id'])
-  print(vita_input['run_date'])
-  print(vita_input['user'])
-  print()
-  print('Plasma Settings:')
-  print('- Isotopes -> {}'.format(vita_input['plasma-settings']['isotopes']))
-  print('- NBI Power -> {}'.format(vita_input['plasma-settings']['heating']['NBI-power']))
-  print('- Ohmic-power -> {}'.format(vita_input['plasma-settings']['heating']['Ohmic-power']))
-  print('- rf-power -> {}'.format(vita_input['plasma-settings']['heating']['rf-power']))
-  print(vita_input['analysis'])
-  print()
+    with open(ARGS.file.name, 'r') as fh:
+        VITA_INPUT = json.load(fh)
 
-  if vita_input['analysis'][0]['mode'] == "midplane_power":
-    run_midplane_power(vita_input['analysis'][0]['type'], vita_input['plasma-settings'])
+    print()
+    print(VITA_INPUT['run_id'])
+    print(VITA_INPUT['run_date'])
+    print(VITA_INPUT['user'])
+    print()
+    print('Plasma Settings:')
+    print('- Isotopes -> {}'.format(VITA_INPUT['plasma-settings']['isotopes']))
+    print('- NBI Power -> {}'.format(VITA_INPUT['plasma-settings']['heating']['NBI-power']))
+    print('- Ohmic-power -> {}'.format(VITA_INPUT['plasma-settings']['heating']['Ohmic-power']))
+    print('- rf-power -> {}'.format(VITA_INPUT['plasma-settings']['heating']['rf-power']))
+    print(VITA_INPUT['analysis'])
+    print()
 
+    if VITA_INPUT['analysis'][0]['mode'] == "midplane_power":
+        run_midplane_power(VITA_INPUT['analysis'][0]['type'], VITA_INPUT['plasma-settings'])
