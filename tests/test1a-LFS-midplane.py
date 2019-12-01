cc20268ac00b06be105b5014539843ca338826ac
Daniel Iglesias <diglesias78@gmail.com>
2019-12-01 17:33:37 +0000

    Slightly modified test1

---
+++ tests/test1a-LFS-midplane.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 18:28:22 2019
+
+@author: Daniel.Ibanez
+"""
+from vita.modules.sol_heat_flux.eich import Eich
+import numpy as np
+
+footprint = Eich(2.5,0.5)# lambda_q=2.5, S=0.5
+
+x=np.linspace(-1,10,100)
+footprint.set_coordinates(x)
+footprint.s_disconnected_dn_max = 2.1
+footprint.fx_in_out = 5.
+
+footprint.calculate_heat_flux_density("hfs")
+footprint.plot_heat_power_density()
+print(footprint.calculate_heat_power())
