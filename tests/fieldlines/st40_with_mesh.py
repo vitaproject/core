72a35328a84115d41e59089e4c667a16ae492a6c
Jeppe Olsen <jmbols@fysik.dtu.dk>
2019-12-17 11:56:41 +0000

    Now makes a loop to calculate field lines and saves output as .csv

--- tests/fieldlines/st40_with_mesh.py
+++ tests/fieldlines/st40_with_mesh.py
@@ -1,9 +1,8 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
 from mayavi import mlab
 from raysect.core import Point3D, World
-from raysect.primitive import import_ply
+from raysect.primitive import import_obj
 
 from vita.modules.cherab import FieldlineTracer, RK2
 from vita.modules.fiesta import Fiesta
@@ -12,29 +11,35 @@ from vita.utility import get_resource
 
 # the world scene-graph
 world = World()
-ST40_mesh = get_resource("ST40", "MESH", "IVC_190522")
-import_ply(ST40_mesh, scaling=0.001, parent=world)
+ST40_mesh = get_resource("ST40", "mesh", "ST40_IVC")
+import_obj(ST40_mesh, scaling=0.001, parent=world)
 
 
-eq002 = get_resource("ST40", "equilibrium", "eq002")
-fiesta = Fiesta(eq002)
+eq001 = get_resource("ST40", "equilibrium", "limited_eq001_export")
+fiesta = Fiesta(eq001)
 b_field = fiesta.b_field
 
 
-seed_points = [
-    Point3D(0.507, 0, 0),
-    Point3D(0.6, 0, 0),
-    Point3D(0.7, 0, 0),
-    Point3D(0.733, 0, -0.01)
-]
+seed_points = []
+
+n = 20
+for i in range(n):
+    lcfs = 0.72677891
+    seed_points.append(Point3D(lcfs + i*0.001, 0.0, 0.0 + 0.02))
+        
+        #Point3D(0.7270, 0, 0),
+        #Point3D(0.7275, 0, 0),
+        #Point3D(0.7280, 0, 0)]
 
 
-field_tracer = FieldlineTracer(b_field, method=RK2(step_size=0.0001))
+field_tracer = FieldlineTracer(b_field, method=RK2(step_size=0.00005))
 
-end_point, trajectory1 = field_tracer.trace(world, seed_points[0], save_trajectory=True, max_length=3)
-end_point, trajectory2 = field_tracer.trace(world, seed_points[1], save_trajectory=True, max_length=5)
-end_point, trajectory3 = field_tracer.trace(world, seed_points[2], save_trajectory=True, max_length=15)
-end_point, trajectory4 = field_tracer.trace(world, seed_points[3], save_trajectory=True, max_length=15)
+end_points = []
+trajectories = []
+for i in range(n):
+    end_point, _, trajectory = field_tracer.trace(world, seed_points[i], save_trajectory=True, max_length=10)
+    trajectories.append(trajectory)
+    end_points.append(end_point)
 
 # mlab.plot3d([0, 0.005, 0.001], [0, 0, 0], [0, 0, 0], tube_radius=0.0005, color=(1, 0, 0))
 # mlab.plot3d([0, 0, 0], [0, 0.005, 0.001], [0, 0, 0], tube_radius=0.0005, color=(0, 1, 0))
@@ -44,8 +49,17 @@ end_point, trajectory4 = field_tracer.trace(world, seed_points[3], save_trajecto
 from raysect_mayavi import visualise_scenegraph
 visualise_scenegraph(world)
 
-mlab.plot3d(trajectory1[:, 0], trajectory1[:, 1], trajectory1[:, 2], tube_radius=0.0005, color=(1, 0, 0))
-mlab.plot3d(trajectory2[:, 0], trajectory2[:, 1], trajectory2[:, 2], tube_radius=0.0005, color=(0.5, 0.5, 0))
-mlab.plot3d(trajectory3[:, 0], trajectory3[:, 1], trajectory3[:, 2], tube_radius=0.0005, color=(0, 1, 0))
-mlab.plot3d(trajectory4[:, 0], trajectory4[:, 1], trajectory4[:, 2], tube_radius=0.0005, color=(0, 0, 1))
+for i in range(n):
+    mlab.plot3d(trajectories[i][:, 0], trajectories[i][:, 1], trajectories[i][:, 2], tube_radius=0.0005)#) color=(1, 0, 0))
+    savename = 'fieldline' + str(i) + '.csv'
+    np.savetxt(savename, (trajectories[i][:, 0], trajectories[i][:, 1], trajectories[i][:, 2]), delimiter=',')
+#mlab.plot3d(trajectory2[:, 0], trajectory2[:, 1], trajectory2[:, 2], tube_radius=0.0005, color=(0.5, 0.5, 0))
+#mlab.plot3d(trajectory3[:, 0], trajectory3[:, 1], trajectory3[:, 2], tube_radius=0.0005, color=(0, 1, 0))
+#mlab.plot3d(trajectory4[:, 0], trajectory4[:, 1], trajectory4[:, 2], tube_radius=0.0005, color=(0, 0, 1))
+
+#np.savetxt('fieldline1.csv', (trajectory1[:, 0], trajectory1[:, 1], trajectory1[:, 2]), delimiter=',')
+#np.savetxt('fieldline2.csv', (trajectory2[:, 0], trajectory2[:, 1], trajectory2[:, 2]), delimiter=',')
+#np.savetxt('fieldline3.csv', (trajectory3[:, 0], trajectory3[:, 1], trajectory3[:, 2]), delimiter=',')
+#np.savetxt('fieldline4.csv', (trajectory4[:, 0], trajectory4[:, 1], trajectory4[:, 2]), delimiter=',')
+
 
