639449d548da91cf9881c15e92700a159e180e54
Jeppe Olsen <jmbols@fysik.dtu.dk>
2020-03-16 11:09:57 +0000

    Updated test4a-map-psi.py to conform to PEP8 and to not require a HESEL file to run

--- tests/2Dprojections/test4a-map-psi.py
+++ tests/2Dprojections/test4a-map-psi.py
@@ -8,43 +8,44 @@ Created on Mon Jan 20 16:37:33 2020
 import numpy as np
 import matplotlib.pyplot as plt
 from vita.modules.fiesta.fiesta_interface import Fiesta
-from vita.modules.sol_heat_flux.hesel_data import HESELdata
 from vita.modules.projection2D.map_psi import map_psi_omp_to_divertor
 from vita.utility import get_resource
-
-def load_x_axis(file_name):
-    '''
-    Function for loading HESEL data
-    '''
-    hesel_data = HESELdata(file_name)
-    hesel_data._load_file()
-
-    i_after_lcfs = np.where(hesel_data.hesel_params.xaxis >= 0)[0]
-
-    x_after_lcfs = hesel_data.hesel_params.xaxis[i_after_lcfs]
-
-    return x_after_lcfs
-
-def load_fiesta_equilibrium(file_name):
-    '''
-    Load Fiesta eqiulibrium
-    '''
-    fiesta = Fiesta(file_name)
-
-    return fiesta
+from vita.modules.sol_heat_flux.eich import Eich
 
 if __name__ == '__main__':
-    filename_hesel = '/media/jmbols/Data/jmbols/ST40/Programme 3/Te(i)_grad_scan/ST40.00003.20.h5'
-    eq002 = get_resource("ST40", "equilibrium", "eq002")
+    FILEPATH = get_resource("ST40", "equilibrium", "eq002")
 
-    x_axis = load_x_axis(filename_hesel)[:800]
-    fiesta = load_fiesta_equilibrium(eq002)
-    x_axis += fiesta.get_midplane_lcfs()[1]
+    FIESTA = Fiesta(FILEPATH)
 
-    divertor_coords_x = np.array([0.375, 0.675])
-    divertor_coords_y = np.array([-0.78, -0.885])
-    divertor_coords = np.array([divertor_coords_x, divertor_coords_y])
-    divertor_map = map_psi_omp_to_divertor(x_axis, fiesta, divertor_coords)
-    r_div = divertor_map["R_div"]
-    angles = divertor_map["Angles"]
-    plt.plot(r_div, angles)
+    MID_PLANE_LOC = FIESTA.get_midplane_lcfs()[1]
+    # specify and load heatflux profile
+    FOOTPRINT = Eich(0.0025, 0.0005, r0_lfs=MID_PLANE_LOC)  # lambda_q=2.5, S=0.5
+
+    X_OMP = np.linspace(0, 10, 100)*1e-3
+    FOOTPRINT.set_coordinates(X_OMP)
+    FOOTPRINT.s_disconnected_dn_max = 2.1
+    FOOTPRINT.fx_in_out = 5.
+    FOOTPRINT.calculate_heat_flux_density("lfs")
+
+    Q_PARALLEL = FOOTPRINT._HeatLoad__q
+    X_AFTER_LCFS = FOOTPRINT.get_global_coordinates()
+
+    DIVERTOR_COORDS_X = np.array([0.375, 0.675])
+    DIVERTOR_COORDS_Y = np.array([-0.78, -0.885])
+    DIVERTOR_COORDS = np.array([DIVERTOR_COORDS_X, DIVERTOR_COORDS_Y])
+    DIVERTOR_MAP = map_psi_omp_to_divertor(X_AFTER_LCFS, FIESTA, DIVERTOR_COORDS)
+    R_DIV = DIVERTOR_MAP["R_div"]
+    Z_DIV = DIVERTOR_MAP["Z_div"]
+    ANGLES = DIVERTOR_MAP["Angles"]
+    F_X = DIVERTOR_MAP["Flux_expansion"]
+    plt.figure()
+    plt.plot(R_DIV, ANGLES)
+
+    plt.figure()
+    plt.plot(R_DIV, F_X)
+
+    plt.figure()
+    plt.plot(R_DIV, Z_DIV)
+
+    plt.figure()
+    plt.plot(R_DIV, Q_PARALLEL*X_AFTER_LCFS/(R_DIV*F_X/np.sin(ANGLES)))
