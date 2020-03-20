0cac199bb2857cb5dcd37b333a55bd038332de26
Jeppe Olsen <jmbols@fysik.dtu.dk>
2020-03-20 13:11:28 +0000

    Fixed a bug, where the LFS coordinates were used

--- tests/2Dprojections/test1b-HFS-midplane.py
+++ tests/2Dprojections/test1b-HFS-midplane.py
@@ -10,12 +10,11 @@ import numpy as np
 
 footprint = Eich(lambda_q=2.5e-3, S=0.5e-3, r0_lfs=0.75, r0_hfs=0.2)# lambda_q=2.5, S=0.5
 
-x=np.linspace(-1,10,1000)*1e-3
+x=np.linspace(-1, 10, 1000)*1e-3
 footprint.s_disconnected_dn_max = 1.0e-3
 footprint.f_x_in_out = 5.0
 footprint.set_coordinates(x*footprint.f_x_in_out)
 q_0 = 1e6
-
 
 footprint.calculate_heat_flux_density("hfs-mp")
 footprint.plot_heat_power_density()
