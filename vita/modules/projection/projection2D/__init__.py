be0a9bbd5aa205c1d9b77d6eda08b5d16417b434
Jeppe Olsen <jmbols@fysik.dtu.dk>
2020-03-26 11:03:40 +0000

    Corrected paths to fit new structure

--- vita/modules/projection/projection2D/__init__.py
+++ vita/modules/projection/projection2D/__init__.py
@@ -5,7 +5,8 @@ Created on Mon Oct 21 09:51:09 2019
 @author: Daniel.Ibanez
 """
 from .field_line.field_line import FieldLine
-from .field_line.field_line_projection import project_heat_flux
+from .field_line.field_line_projection import project_field_lines
 from .field_line.map_field_lines import map_field_lines
-from .map_psi import map_psi_omp_to_divertor
+from .psi_map_projection import map_psi_omp_to_divertor
 from .particle_path_projection import ParticlePath
+from .project_heat_flux import project_heat_flux
