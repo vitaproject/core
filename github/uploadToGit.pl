6f18f28d8bd1a11ce7242a514dd72576e84e92e6
philip r brenan <philiprbrenan@gmail.com>
2020-04-19 12:28:33 +0100

    Vita_on_2020_04_19_at_12_28_33

--- github/uploadToGit.pl
+++ github/uploadToGit.pl
@@ -18,7 +18,7 @@ my @ppi  = (q(-I /home/phil/perl/cpan/DataTableText/lib/),                      
             q(-I /home/phil/perl/cpan/GitHubCrud/lib));
 
 my @html = qw(.html .css);                                                      # File types we want to upload to web page
-my @code = qw(.gitignore .py .pl .perl .yml);                                   # File types we want to upload to vita
+my @code = qw(.gitignore .md .py .pl .perl .yml);                               # File types we want to upload to vita
 
 my $user =  q(philiprbrenan);                                                   # Owner of web page repository
 my $repo = qq($user.github.io);                                                 # Web page repository
