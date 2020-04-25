096a7e1d9813c14d60b261cf2ef0918577a6071f
Daniel Iglesias <diglesias78@gmail.com>
2020-04-25 10:25:56 +0100

    Update uploadToGit.pl
    Forcing the action to work on actions branch

--- github/uploadToGit.pl
+++ github/uploadToGit.pl
@@ -42,6 +42,8 @@ if (1)                                                                          
    }
 
   lll qx(git pull origin master);                                               # Retrieve latest version from repo
+  lll qx(git checkout actions);                                                 # Move to actions branch
+  lll qx(git merge master);                                                     # Update actions with latest master version 
 
   if ($prepareForPullRequest)
    {owf(fpe($home, qw(.github control prepareForPullRequest txt)), q(AAA));     # Request preparation for pull request
