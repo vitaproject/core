ffec5d7a5aec3843adc30ac79f45c35a3b85ad8e
mattngc <matthewncarr@gmail.com>
2020-04-26 22:21:18 +0100

    Adding a guard in the __init__ in case Cherab is not installed. Switching back to default unittest config.

--- vita/modules/cherab/__init__.py
+++ vita/modules/cherab/__init__.py
@@ -1,5 +1,13 @@
 
-from .machine_configuration import load_wall_configuration
-from .b_field import MagneticField
-from .tracer import FieldlineTracer, Euler, RK2, RK4
-from .interface_surface import InterfaceSurface, sample_power_at_surface
+# Note: Cherab sub-module classes and functions will only be available if Vita has been
+# installed with the --inlcude-cherab option.
+
+try:
+
+    from .machine_configuration import load_wall_configuration
+    from .b_field import MagneticField
+    from .tracer import FieldlineTracer, Euler, RK2, RK4
+    from .interface_surface import InterfaceSurface, sample_power_at_surface
+
+except ImportError:
+    pass
