1e2974c3dcdd173d22877108dcb0e3e66c2cda18
Jeppe Olsen <jmbols@fysik.dtu.dk>
2020-08-17 14:42:21 +0200

    Changed the LCFS from being at psi_n = 1.005 to psi_n = 1.0005

--- vita/modules/equilibrium/fiesta/fiesta_interface.py
+++ vita/modules/equilibrium/fiesta/fiesta_interface.py
@@ -46,16 +46,26 @@ class Fiesta:
 
         input: self, a reference the object itself
 
-        output: self.r_limiter, a numpy array with the radial coordinates of the vessel limits
-                self.z_limiter, a numpy array with the vertical coordinates of the vessel limits
-                self.r_vec,     a numpy array with the radial grid coordinates
-                self.z_vec,     a numpy array with the vertical grid coordinates
-                self.psi_n,
-                self.b_r,       a numpy array with the radial magnetic field component
-                self.b_z        a numpy array with the vertical field component
-                self.b_phi,     a numpy array with the toroidal field component
-                self.b_theta,   a numpy array with the poloidal magnetic field component
-                self.i_rod      a float with the current in the rod
+        output: self.r_limiter,    a numpy array with the radial coordinates of the vessel limits
+                self.z_limiter,    a numpy array with the vertical coordinates of the vessel limits
+                self.r_vec,        a numpy array with the radial grid coordinates
+                self.z_vec,        a numpy array with the vertical grid coordinates
+                self.psi,          a numpy array with the flux coordinates
+                self.psi_n,        a numpy array with the normalised flux coordinate
+                self.psi_axis,
+                self.psi_lcfs,
+                self.mag_axis,
+                self.b_r,          a numpy array with the radial magnetic field component
+                self.b_z           a numpy array with the vertical field component
+                self.b_phi,        a numpy array with the toroidal field component
+                self.b_theta,      a numpy array with the poloidal magnetic field component
+                self.b_vac_radius, a
+                self.b_vac,
+                self.i_rod,        a float with the current in the rod
+                self.x_points,
+                self.f_profile,
+                self.q_profile,
+                self.lcfs_polygon,
         '''
         # Read data from .mat file
         mat = sio.loadmat(self.filename, mat_dtype=True, squeeze_me=True)
@@ -67,10 +77,10 @@ class Fiesta:
         # Get grid data
         self.r_vec = mat['r']
         self.z_vec = mat['z']
+        self.psi_n = mat['psi_n']
 
         # Get magnetic data
         self.psi = mat['psi']
-        self.psi_n = mat['psi_n']
         self.psi_axis = mat['psi_a']
         self.psi_lcfs = mat['psi_b']
         self.mag_axis = mat['mag_axis']
@@ -86,7 +96,7 @@ class Fiesta:
         self.q_profile = mat['q_profile']
         self.lcfs_polygon = mat['lcfs_polygon']
 
-    def get_midplane_lcfs(self, psi_p=1.005):
+    def get_midplane_lcfs(self, psi_p=1.0005):
         '''
         Function for getting the inner and outer radial position of the LCFS at the midplane
 
@@ -96,8 +106,8 @@ class Fiesta:
 
         return: Rcross, a list with the outer and inner radial position of the mid-plane LCFS
         '''
-
         r_vec, z_vec = np.meshgrid(self.r_vec, self.z_vec)
+
         # Get contour
         cont = plt.contour(r_vec, z_vec, self.psi_n, [psi_p])
         cont = cont.allsegs[0]
@@ -135,8 +145,8 @@ class Fiesta:
         except ImportError:
             raise RuntimeError("CHERAB integration not installed.")
 
-        r_vec = self.r_vec
-        z_vec = self.z_vec
+        r_vec, z_vec = self.r_vec, self.z_vec
+
         psi = np.swapaxes(self.psi, 0, 1)
         psi_axis = self.psi_axis
         psi_lcfs = self.psi_lcfs
