4cd99ba63a1f573c9f7a53c371e999e39ef764a0
Jeppembo <jmbols@fysik.dtu.dk>
2020-12-15 10:31:30 +0100

    Working on implementing 2D mapping for an arbitrary surface

--- vita/modules/projection/projection2D/interp_div.py
+++ vita/modules/projection/projection2D/interp_div.py
@@ -6,12 +6,13 @@ Created on Thu Dec 10 14:24:20 2020
 @author: jmbols
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_div_coords(divertor_coords):
     # Define the 1D polynomial that represents the divertor
-    for divertor_coord in divertor_coords:
-        divertor_x = divertor_coord[0]
-        divertor_y = divertor_coord[1]
+    for i in range(len(divertor_coords[0, :])-1):
+        divertor_x = [divertor_coords[0, i], divertor_coords[0, i+1]]
+        divertor_y = [divertor_coords[1, i], divertor_coords[1, i+1]]
 
         divertor_func = np.polyfit(divertor_x, divertor_y, 1)
         divertor_polyfit = np.poly1d(divertor_func)
@@ -25,4 +26,21 @@ def get_div_coords(divertor_coords):
         # for evaluating the angle of incidence)
         divertor_func_below = [divertor_func[0], divertor_func[1] - 0.001]
         divertor_polyfit_below = np.poly1d(divertor_func_below)
+
+def get_z_from_r(r):
+    z = r
+    return z
+
+if __name__ == '__main__':
+    divertor_coords_x = [0.252, 0.332, 0.332, 0.368]
+    divertor_coords_y = [-0.426, -0.621, -0.710, -0.806]
+    divertor_coords = np.array([divertor_coords_x, divertor_coords_y])
+    #divertor_coords_x = [0.226, 0.314, 0.365, 0.406]
+    #divertor_coords_y = [-0.425, -0.621, -0.708, -0.811]
+
+    get_div_coords(divertor_coords)
+
+    plt.plot(divertor_coords_x, divertor_coords_y)
     
+    r = 5
+    function_z_from_r = get_z_from_r(r)
